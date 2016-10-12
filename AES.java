fc5ed612faa257331cc8fe779747274e5a508097
Zev Isert <zev.isert@gmail.com>
2016-10-11 18:45:43 -0700

    Encryption and decryption finished

--- AES.java
+++ AES.java
@@ -1,5 +1,8 @@
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
@@ -20,8 +23,9 @@ public class AES extends rijndael
 			if (s.equalsIgnoreCase(argsList.get(0)))
 			{
 				// TODO: prepare encrypt here
-				byte[] key = new byte[16];
-				File file = null;
+				byte[] key = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+
+				File file = new File(argsList.get(2));
 				AES cypher = new AES();
 				cypher.encrypt(key, file);
 			}
@@ -32,54 +36,64 @@ public class AES extends rijndael
 			if (s.equalsIgnoreCase(argsList.get(0)))
 			{
 				// TODO: prepare decrypt here
-				byte[] key = new byte[16];
-				File file = null;
-				
+				byte[] key = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+
+				File file = new File(argsList.get(2));
 				AES cypher = new AES();
 				cypher.decrypt(key, file);
 			}
 		}
 	}
 
+	public static String bytesToHex(byte[] in) {
+	    final StringBuilder builder = new StringBuilder();
+	    for(byte b : in) {
+	        builder.append(String.format("%02x", b));
+	    }
+	    return builder.toString();
+	}
+	
 	public void encrypt(byte[] key, File plaintext)
 	{
 		System.out.println(">>> encrypting");
 		assert false : "Not implemented";
 		
-		String s = "Stri"+
-				   "ng o"+
-				   "f 16"+
-				   "char";
-
-		byte[] test_data = s.getBytes();
+		char[] buffer = new char[(int) plaintext.length()];
 		
-		State state = new State(test_data);
-		// Key Expansion
-		// keyExpansion();
+		try (FileReader reader = new FileReader(plaintext)){
+			reader.read(buffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		state.addRoundKey(key);
+		String contents = new String(buffer);
 		
-		int numberOfRounds = 1;
+		String block = contents.substring(0, 16);
 		
-		for(int round = 1; round <= numberOfRounds; ++round)
+		State state = new State(block.getBytes());
+		
+		state.keyExpansion(key);
+		
+		state.addRoundKey(state.keys[state.round]);
+		int numberOfRounds = 10;
+		
+		for(state.round = 1; state.round < numberOfRounds; ++state.round)
 		{
 			state.subBytes();
 			state.shiftRows();
 			state.mixColumns();
-			state.addRoundKey(key);
+			state.addRoundKey(state.keys[state.round]);
+
 		}
 		
 		// Final round
 		state.subBytes();
 		state.shiftRows();
-		state.addRoundKey(key);
+		state.addRoundKey(state.keys[state.round]);
 		
-		System.out.print("[");
-		for(byte b : state.data)
-		{
-			System.out.print(Integer.toHexString(Byte.toUnsignedInt(b)).toUpperCase() + ", ");
-		}
-		System.out.println("]");
+		System.out.println(bytesToHex(state.data));
+
 	}
 
 	public void decrypt(byte[] key, File encrpyted)
@@ -87,21 +101,37 @@ public class AES extends rijndael
 		System.out.println("<<< decrpyting");
 		assert false : "Not implemented";
 		
-		byte[] test_data = {(byte)0xB5, 0x12, 0x59, 0x12, (byte)0xA1, 0x3E, 0x7D, 0x28, (byte)0x83, 0x7B, 0x27, 0x2B, (byte)0x89, 0x28, 0x54, (byte)0xAF};
+
+		byte[] array = null;
 		
-		State state = new State(test_data);
+		try {
+			array = Files.readAllBytes(encrpyted.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		byte[] chunk = new byte[16];
+		for (int i = 0; i < 16; ++i)
+		{
+			chunk[i] = (byte)array[i];
+		}
+		
+		State state = new State(chunk);
+		
 		// Key Expansion
-		// keyExpansion();
+		state.keyExpansion(key);
 		
-		state.addRoundKey(key);
+		int numberOfRounds = 10;
+					
+		state.addRoundKey(state.keys[numberOfRounds]);
 		
-		int numberOfRounds = 1;
-		
-		for(int round = 1; round <= numberOfRounds; ++round)
+		for(state.round = numberOfRounds - 1; state.round > 0; --state.round)
 		{
 			state.InverseShiftRows();
 			state.InverseSubBytes();
-			state.addRoundKey(key);
+			state.addRoundKey(state.keys[state.round]);
 			state.InverseMixColumns();
 		}
 		
@@ -110,9 +140,8 @@ public class AES extends rijndael
 		state.InverseShiftRows();
 		state.InverseSubBytes();
 
-		state.addRoundKey(key);
+		state.addRoundKey(state.keys[state.round]);
 		
-		String s = new String(state.data);
-		System.out.println(s);
+		System.out.println("output: " + new String(state.data));
 	}
 }
